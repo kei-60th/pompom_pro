3d6fee6f351cc73c4c76e2ebed47e2e1fa361339
kei-60th <kei-1005@i.softbank.jp>
2019-09-24 14:05:28 +0900

    show tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,8 +2,26 @@ class TasksController < ApplicationController
 
   def index
     @task = Task.new
+    @tasks = Task.order('updated_at DESC')
   end
 
   def create
+    @task = Task.new(task_params)
+
+    if @task.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+
+
+  private
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def task_params
+    params.fetch(:task, {}).permit(
+        :name, :is_done
+    )
   end
 end
