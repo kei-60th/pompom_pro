62f8ae15a307efec0eb1088995f7b4fb17810127
kei-60th <kei-1005@i.softbank.jp>
2019-09-25 09:37:23 +0900

    task posts

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,19 +2,33 @@ class TasksController < ApplicationController
 
   def index
     @task = Task.new
-    @tasks = Task.order('updated_at DESC')
+    @tasks = Task.where("is_done != 1").order('updated_at DESC')
+    @endtasks = Task.where("is_done = 1").order('updated_at DESC')
   end
 
   def create
     @task = Task.new(task_params)
-
     if @task.save
       redirect_to root_path
-    else
-      render :index
     end
   end
 
+  def update
+    @task = Task.find(params[:id])
+    if @task.is_done? 
+      @task.is_done = 0
+    else
+      @task.is_done = 1
+    end
+    unless params.require(:test)=="delete"
+      if @task.update(task_params)
+        redirect_to root_path
+      end
+      else
+        @task.delete
+        redirect_to root_path
+      end
+  end
 
 
   private
