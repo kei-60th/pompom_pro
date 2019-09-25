c82fda6e9893a2d752bdc921d5c2a94cdc1df4ec
kei-60th <kei-1005@i.softbank.jp>
2019-09-25 13:19:13 +0900

    generate timer view

--- app/controllers/timers_controller.rb
+++ app/controllers/timers_controller.rb
@@ -1,5 +1,8 @@
 class TimersController < ApplicationController
 
   def index
+    @tasks = Task.where("is_done != 1").order('updated_at DESC')
+    @endtasks = Task.where("is_done = 1").order('updated_at DESC')
   end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root to: 'tasks#index'
   resources :tasks, only: [:index,:create,:update,:destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :timers, only: [:index]
 end
