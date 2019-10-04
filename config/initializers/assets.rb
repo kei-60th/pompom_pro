360b8af3174b41d27fb44c361ee293001da4048c
kei-60th <kei-1005@i.softbank.jp>
2019-10-05 00:37:28 +0900

    delete require_tree

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,3 +12,7 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+Rails.application.config.assets.precompile += %w( cable.js )
+Rails.application.config.assets.precompile += %w( spa.js )
+Rails.application.config.assets.precompile += %w( tasks.js )
+Rails.application.config.assets.precompile += %w( timers.js )
