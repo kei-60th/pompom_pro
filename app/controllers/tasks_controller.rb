f752b5259b33d9e3d87fe5ca188c080b0bcda541
kei-60th <kei-1005@i.softbank.jp>
2019-10-04 21:29:40 +0900

    postfunction for user

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,14 +3,19 @@ class TasksController < ApplicationController
   def index
     @task = Task.new
     if user_signed_in?
-      @tasks = Task.where("is_done != 1 AND user_id = current_user.id")
-      @endtasks = Task.where("is_done = 1 AND user_id = current_user.id")
+      @tasks = Task.where("is_done != 1 AND user_id = #{current_user.id}")
+      @endtasks = Task.where("is_done = 1 AND user_id = #{current_user.id}")
     else
       @tasks = Task.where("is_done != 1 AND user_id = 1")
       @endtasks = Task.where("is_done = 1 AND user_id = 1")
     end
     @post = Post.new
-    @posts = Post.all.order("id DESC")
+    if user_signed_in?
+      current_id = current_user.id
+    else
+      current_id = 1
+    end
+    @posts = Post.where("user_id = #{current_id}").order("id DESC")
   end
 
   def create
@@ -46,8 +51,15 @@ class TasksController < ApplicationController
   private
   # Never trust parameters from the scary internet, only allow the white list through.
   def task_params
+    if user_signed_in?
+      current_id = current_user.id
+    else
+      current_id = 1
+    end
     params.fetch(:task, {}).permit(
         :name, :is_done
+    ).merge(
+      user_id: current_id
     )
   end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_04_093940) do
+ActiveRecord::Schema.define(version: 2019_10_04_120613) do
 
   create_table "endtasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2019_10_04_093940) do
     t.integer "time", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
