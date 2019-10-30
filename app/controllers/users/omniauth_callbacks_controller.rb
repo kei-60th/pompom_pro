71d145097722e3eabf46e60ce27771c97bdae7f2
kei-60th <kei-1005@i.softbank.jp>
2019-10-30 22:01:04 +0900

    twitterによるログイン機能の実現

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -34,10 +34,26 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def callback_from(provider)
     @sns = User.find_for_oauth(request.env['omniauth.auth'])
-    unless SnsCredential.find_by(user_id: current_user.id)
-      @sns.user_id = current_user.id
-      @sns.save
+    if current_user #ユーザーがログインしている
+      unless SnsCredential.find_by(user_id: current_user.id) #まだsns認証をしていない
+        @sns.user_id = current_user.id
+        @sns.save
+      end
+    else #ユーザーがログインしていない
+      if SnsCredential.find_by(uid: @sns.uid) #sns認証をしたことがある
+        @user_id = SnsCredential.find_by(uid: @sns.uid).user_id
+        @user = User.find_by(id: @user_id)
+      else #sns認証をしたことがない
+        @user = User.create(
+          email: "#{@sns.uid}-#{@sns.provider}@example.com",
+          password:Devise.friendly_token.first(8)
+        )
+        @sns.user_id = @user.id
+        @sns.save
+      end
+      sign_in @user
     end
+
     redirect_to root_path
   end
 
@@ -45,6 +61,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def failure
     redirect_to root_path and return
   end
+
 
 
 end
